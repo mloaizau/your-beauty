1c15b14fcd93ab2bfb886c3a563f03d72abaa98b
mloaizau <mloaiza.urrutia@gmail.com>
2022-06-09 17:37:35 -0400

    enrutando a categorias

--- src/app/secciones/resumen/resumen/resumen.component.ts
+++ src/app/secciones/resumen/resumen/resumen.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { SessionService } from "../../../services/session.service";
-import { IonSlides } from '@ionic/angular';
+import { SessionService } from '../../../services/session.service';
+import { IonSlides, NavController } from '@ionic/angular';
 import { StoreService } from 'src/app/services/store.service';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-resumen',
   templateUrl: 'resumen.component.html',
-  styleUrls: ['resumen.component.scss']
+  styleUrls: ['resumen.component.scss'],
 })
 export class ResumenPage implements OnInit {
-
   @ViewChild(IonSlides) slides: IonSlides;
   public session: any;
   public producto: any;
@@ -17,32 +16,35 @@ export class ResumenPage implements OnInit {
   slideOptsOne = {
     initialSlide: 0,
     slidesPerView: 1,
-    autoplay:true
-   };
+    autoplay: true,
+  };
 
   constructor(
     private _session: SessionService,
     private _storeService: StoreService,
-    private router: Router
-  ) {
-  }
+    private router: Router,
+  ) {}
 
-  ngOnInit(){
+  ngOnInit() {
     this.obtenerSession();
   }
 
-  async obtenerSession(){
-    this.session = await this._session.get("sessionActive");
+  async obtenerSession() {
+    this.session = await this._session.get('sessionActive');
   }
 
-  async ionViewWillEnter(){
-    if(!this.session){
+  async ionViewWillEnter() {
+    if (!this.session) {
       this.obtenerSession();
     }
-    this.producto = await this._storeService.getID("7");
-    console.log(this.producto[0].nombre);
+    this.producto = await this._storeService.getID('7');
   }
-  filtroMarca(marca){
-      this.router.navigate(['/home/tabs/ofertas'], { queryParams: {marca: marca} });
+  filtroMarca(marca) {
+    this.router.navigate(['/home/tabs/ofertas'], {
+      queryParams: { marca: marca },
+    });
+  }
+  goCategories() {
+    this.router.navigate(['/home/tabs/categoria']);
   }
 }
