c42925f9ca1ddca17043ffe291e0de7914e04307
mloaizau <mloaiza.urrutia@gmail.com>
2022-06-11 21:44:28 -0400

    se agrega llamada telefonica

--- src/app/secciones/no-disponible/no-disponible/no-disponible.component.ts
+++ src/app/secciones/no-disponible/no-disponible/no-disponible.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { CallNumber } from '@awesome-cordova-plugins/call-number/ngx';
 @Component({
   selector: 'app-no-disponible',
   templateUrl: './no-disponible.component.html',
@@ -10,7 +10,8 @@ export class NoDisponibleComponent implements OnInit {
 
   public params: any;
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private callNumber: CallNumber
   ) {
     this.route.queryParams.subscribe(respuesta => {
       this.params = respuesta;
@@ -18,5 +19,11 @@ export class NoDisponibleComponent implements OnInit {
    }
 
   ngOnInit() {}
+
+  llamar(){
+    this.callNumber.callNumber("56958400350", true)
+  .then(res => console.log('Launched dialer!', res))
+  .catch(err => console.log('Error launching dialer', err));
+  }
 
 }
